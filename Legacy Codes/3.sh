d284554c557a7c19957ca3e3584e9949cab720ad
Avail064 <58548271+Avail064@users.noreply.github.com>
2019-12-06 20:24:26 +0700

    Update 3.sh

--- Legacy Codes/3.sh
+++ Legacy Codes/3.sh
@@ -1,84 +1,94 @@
+declare -a matriks_satu
+declare -a matriks_dua
+
+while :; do
 clear
-i=0
-declare -a nama
-declare -a npm
-declare -a jur
 
-while :;
-do
- echo -e "Menu : \n\n1. Input\n2. View\n3. Search\n4. Exit\n"
- echo -n "Masukkan pilihan : "
- read pilih
+echo "Program Perkalian Matriks"
+echo "1. Masukkan nilai"
+echo "2. Jumlahkan matriks"
+echo "3. Kalikan matriks"
+echo "4. Exit"
 
- if (("$pilih" == 1));   # INPUT DATA
- then
-  clear
-  echo -n "Masukkan Nama    : "
-  read nama[$i]
-  echo -n "Masukkan NPM     : "
-  read npm[$i]
-  echo -n "Masukkan Jurusan : "
-  read jur[$i]
-  i=`expr $i + 1`
-  clear
+read pilih
 
- elif (("$pilih" == 2));   # TAMPILKAN DATA
- then
-  if (( i == 0))
-  then
-   clear
-   echo "Tidak ada data yang dapat ditampilkan"
-   read
-   clear
-  else
-   clear
-   echo -e "Data yang telah dimasukkan : \n"
-   for (( q=0; q<i;q++  ))
-   do
-    echo -e "Data ke $[q+1]\nNama    : ${nama[q]}\nNPM     : ${npm[q]}\nJurusan : ${jur[q]}\n"
-   done
-   read
-   clear
-  fi
-
- elif (("$pilih" == 3));   # CARI DATA
- then
-  if (( i == 0))
-  then
-   clear
-   echo "Tidak ada data yang dapat dicari"
-   read
-   clear
-  else
-   clear
-      echo -n "Masukkan NPM mahasiswa yang ingin dicari : "
-      read cari
-
-      k=0
-      while (($cari != ${npm[$k]}))
-      do
-    k=`expr $k + 1`
-      done
-
-   if (($cari == ${npm[$k]}));
-   then
-    echo -e "\nNama    : ${nama[k]}\nNPM     : ${npm[k]}\nJurusan : ${jur[k]}"
-
-   else
+if [ $pilih == 1 ]
+then
     clear
-    echo -e "Data tidak ditemukan"
-   fi
-   read
-      clear
-  fi
+    echo "Masukkan nilai pada matriks satu :"
+    for ((i=0; i<=3; i++)) do
+        echo "Nilai ke "$(( $i + 1))
+        read nilai
+        
+        matriks_satu[$i]=$nilai
+    done
+    
+    echo ""
+    echo "Masukkan nilai pada matriks dua :"
+    for ((i=0; i<=3; i++)) do
+        echo "Nilai ke "$(( $i + 1))
+        read nilai
+        
+        matriks_dua[$i]=$nilai
+    done
+fi
 
- elif (( "$pilih" != 4 && "$pilih" != 2 && "$pilih" != 1 && "$pilih" !=       3  ));   # APABILA MEMBERIKAN MASUKAN SELAIN 1-4
- then
-  echo "Pilihan tidak valid"
-  read
-  clear
+if [ $pilih == 2 ]
+then
+    clear
+    
+    declare -a matriks_tiga
+    for ((i=0; i<4; i++)) do
+        let matriks_tiga[$i]=matriks_satu[$i]+matriks_dua[$i]
+    done
+    
+    echo "Hasil Penjumlahan"
+    for ((i=0; i<4; i++)) do
+        echo -n "${matriks_tiga[$i]} "
 
- else    # KELUAR PROGAM
-  exit
- fi
+        if [ $i == 1 ]
+        then
+            echo ""
+        fi
+    done
+    unset matriks_tiga
+    read
+fi
+
+if [ $pilih == 3 ]
+    then
+    clear
+    declare -a matriks_tiga
+
+    for ((i=0; i<2; i++)) do
+        for ((j=0; j<2; j++)) do
+            let matriks_tiga[$i]+=matriks_satu[$j]*matriks_dua[$i+$j*2]
+        done
+    done
+
+    for ((i=0; i<2; i++)) do
+        for ((j=0; j<2; j++)) do
+            let matriks_tiga[$i+2]+=matriks_satu[$j+2]*matriks_dua[$i+$j*2]
+        done    
+    done
+    
+    echo "Hasil perkalian"
+    for ((i=0; i<4; i++)) do
+        echo -n "${matriks_tiga[$i]} "
+
+        if [ $i == 1 ]
+        then
+            echo ""
+        fi
+    done
+    unset matriks_tiga
+    read
+fi
+
+if [ $pilih == 4 ]
+then
+    exit
+fi
+
 done
+
